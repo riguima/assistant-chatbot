a6b2e79cbaeff6c68d9976322620530f9f70bfc3
riguima <richard.alexsander.guima@gmail.com>
2024-04-29 19:25:22 +0000

    whatsapp bot works

--- audio_para_texto/views.py
+++ audio_para_texto/views.py
@@ -1,10 +1,14 @@
-from flask import redirect, render_template, request, url_for
+from flask import redirect, render_template, request, url_for, jsonify
 from flask_login import current_user, login_user, logout_user
 from sqlalchemy import select
+from pathlib import Path
+from httpx import get, post
 
 from audio_para_texto.database import Session
 from audio_para_texto.forms import LoginForm
 from audio_para_texto.models import User
+from audio_para_texto.config import config
+from audio_para_texto.utils import transcribe_audio
 
 
 def init_app(app):
@@ -19,7 +23,6 @@ def init_app(app):
                     .where(User.is_admin == True)
                 )
                 user_model = session.scalars(query).first()
-                print(user_model)
                 if (
                     user_model
                     and user_model.password == request.form['password']
@@ -54,3 +57,35 @@ def init_app(app):
             pass
         logout_user()
         return redirect(url_for('login'))
+
+    @app.route('/', methods=['GET', 'POST'])
+    def test_whatsapp():
+        if request.method == 'GET' and request.args.get('hub.challenge') and request.args.get('hub.verify_token') == config['WHATSAPP_API_TOKEN']:
+            return str(request.args['hub.challenge'])
+        message = request.json['entry'][0]['changes'][0]['value']['messages'][0]
+        if message['type'] == 'audio':
+            audio_id = message['audio']['id']
+            url = get(f'https://graph.facebook.com/v19.0/{audio_id}', headers={
+                'Authorization': f'Bearer {config["WHATSAPP_API_ACCESS_TOKEN"]}'
+            }).json()['url']
+            audio_path = Path('static') / 'audios' / f'{audio_id}.mp3'
+            with open(audio_path, 'wb') as f:
+                response = get(url, headers={
+                    'Authorization': f'Bearer {config["WHATSAPP_API_ACCESS_TOKEN"]}'
+                })
+                f.write(response.content)
+            response = post('https://graph.facebook.com/v19.0/325664330621284/messages', headers={
+                'Authorization': f'Bearer {config["WHATSAPP_API_ACCESS_TOKEN"]}'
+            }, json={
+                'messaging_product': 'whatsapp',
+                'context': {
+                    'message_id': message['id'],
+                },
+                'to': message['from'],
+                'type': 'text',
+                'text': {
+                    'preview_url': False,
+                    'body': transcribe_audio(str(audio_path)),
+                }
+            })
+        return jsonify({'status': 'ok'})
